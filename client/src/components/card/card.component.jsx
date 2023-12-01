47b93c749b540f65b06f4a3ae15906b727101410
EnzoSacinRuiz <Tu email aca>
2023-11-30 23:43:53 -0500

    forms finished

--- client/src/components/card/card.component.jsx
+++ client/src/components/card/card.component.jsx
@@ -1,53 +1,39 @@
-// DogCard.jsx
 import React from 'react';
 import './card.styles.css';
 import { Link } from 'react-router-dom';
 
 function Card({ dog }) {
-  const { name, Temperaments, url, id, height, weight, created} = dog;
-  console.log(Temperaments);
+  const { name, Temperaments, url, id, height, weight, created } = dog;
 
   const renderTemperament = () => {
-    if (Array.isArray(Temperaments)) {
-      if (Temperaments.length === 0) {
-        return 'No temperament information available';
-      } else if (typeof Temperaments[0] === 'string') {
-        // If Temperaments is an array of strings
-        return Temperaments.join(', ');
-      } else if (typeof Temperaments[0] === 'object' && 'name' in Temperaments[0]) {
-        // If Temperaments is an array of objects with 'name' property
-        const temperamentNames = Temperaments.map(temp => temp.name);
-        return temperamentNames.join(', ');
-      }
+    if (!Temperaments || Temperaments.length === 0) {
+      return 'No temperament information available';
+    } else if (Array.isArray(Temperaments) && typeof Temperaments[0] === 'string') {
+      return Temperaments.join(', ');
+    } else if (Array.isArray(Temperaments) && typeof Temperaments[0] === 'object' && 'name' in Temperaments[0]) {
+      const temperamentNames = Temperaments.map(temp => temp.name);
+      return temperamentNames.join(', ');
     } else {
       return 'Temperament data not available';
     }
   };
-  
 
+  
   return (
     <Link to={`/home/${id}`}>
       <div className="dog-card-container">
-        <div className="dog-card-content">
-        {created ? (
-            <img
-              src={url}
-              alt={`${name}`}
-              className="dog-card-image"
-            />
-          ) : (
-            <img
-              src={`https://cdn2.thedogapi.com/images/${url}.jpg`}
-              alt={`${name}`}
-              className="dog-card-image"
-            />
-          )}
-          <div className="dog-card-text">
-            <h2>Raza: {name}</h2>
-            <p>Altura: {height}</p>
-            <p>Peso: {weight}</p>
-            <p> Temperamento: {renderTemperament()}</p>
-          </div>
+        <h2>{name}</h2>
+        {url && (
+          <img
+            src={created ? url : `https://cdn2.thedogapi.com/images/${url}.jpg`}
+            alt={`${name}`}
+            className="dog-card-image"
+          />
+        )}
+        <div className="dog-card-text">
+          <p>Altura: {height}</p>
+          <p>Peso: {weight}</p>
+          <p>Temperamento: {renderTemperament()}</p>
         </div>
       </div>
     </Link>
--- client/src/views/create/create.component.jsx
+++ client/src/views/create/create.component.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import "./create.styles.css"
 
-const Create = () =>  {
+const Create = () => {
   const [name, setName] = useState('');
   const [minHeight, setMinHeight] = useState('');
   const [maxHeight, setMaxHeight] = useState('');
@@ -9,123 +10,207 @@ const Create = () =>  {
   const [lifeSpan, setLifeSpan] = useState('');
   const [temperament, setTemperament] = useState('');
 
+  const [errors, setErrors] = useState({
+    name: '',
+    minHeight: '',
+    maxHeight: '',
+    minWeight: '',
+    maxWeight: '',
+    lifeSpan: '',
+    temperament: '',
+  });
+  const [formValid, setFormValid] = useState(false);
+  const [alertError, setAlertError] = useState(false); 
+
+  useEffect(() => {
+    const validateForm = () => {
+      const errorsCopy = {};
+      errorsCopy.name = !name ? 'Name is required.' : '';
+      errorsCopy.minHeight = !minHeight ? 'Min Height is required.' : '';
+      errorsCopy.maxHeight = !maxHeight ? 'Max Height is required.' : '';
+      errorsCopy.minWeight = !minWeight ? 'Min Weight is required.' : '';
+      errorsCopy.maxWeight = !maxWeight ? 'Max Weight is required.' : '';
+
+      if (isNaN(parseInt(minHeight))) {
+        errorsCopy.minHeight = 'Min Height should be a number.';
+      }
+
+      if (isNaN(parseInt(maxHeight))) {
+        errorsCopy.maxHeight = 'Max Height should be a number.';
+      }
+
+      if (isNaN(parseInt(minWeight))) {
+        errorsCopy.minWeight = 'Min Weight should be a number.';
+      }
+
+      if (isNaN(parseInt(maxWeight))) {
+        errorsCopy.maxWeight = 'Max Weight should be a number.';
+      }
+
+
+      setErrors(errorsCopy);
+
+
+      const isNameValid = errorsCopy.name === '';
+      const isMinHeightValid = errorsCopy.minHeight === '';
+      const isMaxHeightValid = errorsCopy.maxHeight === '';
+      const isMinWeightValid = errorsCopy.minWeight === '';
+      const isMaxWeightValid = errorsCopy.maxWeight === '';
+
+      const isValid =
+        isNameValid &&
+        isMinHeightValid &&
+        isMaxHeightValid &&
+        isMinWeightValid &&
+        isMaxWeightValid;
+
+      setFormValid(isValid);
+    };
+
+    validateForm();
+  }, [name, minHeight, maxHeight, minWeight, maxWeight]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Validation
-    if (
-      !name ||
-      !minHeight ||
-      !maxHeight ||
-      !minWeight ||
-      !maxWeight ||
-      !lifeSpan ||
-      !temperament
-    ) {
-      alert('All fields are required.');
-      return;
-    }
 
     if (
       parseInt(minHeight) >= parseInt(maxHeight) ||
       parseInt(minWeight) >= parseInt(maxWeight)
     ) {
-      alert('Maximum value should be greater than the minimum value for height and weight.');
+      setAlertError(true); // Activate the alert error state
       return;
     }
 
-    if (
-      !Number.isInteger(parseInt(minHeight)) ||
-      !Number.isInteger(parseInt(maxHeight)) ||
-      !Number.isInteger(parseInt(minWeight)) ||
-      !Number.isInteger(parseInt(maxWeight)) ||
-      !Number.isInteger(parseInt(lifeSpan))
-    ) {
-      alert('Height, weight, and life span must be integers.');
-      return;
-    }
+    if (formValid) {
+      setAlertError(false); // Reset the alert error state if validation passes
+      try {
+        // Your code for handling form submission
+        const response = await fetch('http://localhost:3001/dogs', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name,
+            height: `${parseInt(minHeight)} - ${parseInt(maxHeight)}`,
+            weight: `${parseInt(minWeight)} - ${parseInt(maxWeight)}`,
+            life_span: parseInt(lifeSpan),
+            url: 'lolol',
+            temperamentName: temperament,
+          }),
+        });
 
-    try {
-      // API call to your backend
-      const response = await fetch('http://localhost:3001/dogs', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name,
-          height: `${parseInt(minHeight)} - ${parseInt(maxHeight)}`,
-          weight: `${parseInt(minWeight)} - ${parseInt(maxWeight)}`,
-          life_span: parseInt(lifeSpan),
-          url: 'lolol',
-          temperamentName: temperament,
-        }),
-      });
-
-      if (response.ok) {
-        alert('Dog breed created successfully!');
-        // Reset form fields after successful submission if needed
-        setName('');
-        setMinHeight('');
-        setMaxHeight('');
-        setMinWeight('');
-        setMaxWeight('');
-        setLifeSpan('');
-        setTemperament('');
-      } else {
-        alert('Failed to create dog breed.');
+        if (response.ok) {
+          alert('Dog breed created successfully!');
+          // Reset all input fields
+          setName('');
+          setMinHeight('');
+          setMaxHeight('');
+          setMinWeight('');
+          setMaxWeight('');
+          setLifeSpan('');
+          setTemperament('');
+        } else {
+          try {
+            const responseData = await response.json();
+            if (
+              responseData.error &&
+              responseData.error.includes('llave duplicada viola restricción de unicidad «Dogs_name_key1»')
+            ) {
+              alert('This name already exists. Please choose a different name.');
+            } else {
+              alert('Failed to create dog breed.');
+            }
+          } catch (error) {
+            console.error('Error parsing response data:', error);
+            alert('An error occurred while processing the response.');
+          }
+        }
+      } catch (error) {
+        console.error('Error creating dog breed:', error);
+        alert('An error occurred while creating the dog breed.');
       }
-    } catch (error) {
-      console.error('Error creating dog breed:', error);
-      alert('An error occurred while creating the dog breed.');
+    } else {
+      alert('Please fill out the form correctly.');
     }
   };
 
+
+
+  // const errorList = Object.values(errors)
+  // console.log(errorList);
+
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="form-container" onSubmit={handleSubmit}>
       <input
         type="text"
         value={name}
         onChange={(e) => setName(e.target.value)}
         placeholder="Name"
       />
+      {errors.name && <span className="error">{errors.name}</span>}
+
       <input
         type="text"
         value={minHeight}
         onChange={(e) => setMinHeight(e.target.value)}
         placeholder="Min Height"
       />
+      {errors.minHeight && <span className="error">{errors.minHeight}</span>}
+
       <input
         type="text"
         value={maxHeight}
         onChange={(e) => setMaxHeight(e.target.value)}
         placeholder="Max Height"
       />
+      {errors.maxHeight && <span className="error">{errors.maxHeight}</span>}
+
       <input
         type="text"
         value={minWeight}
         onChange={(e) => setMinWeight(e.target.value)}
         placeholder="Min Weight"
       />
+      {errors.minWeight && <span className="error">{errors.minWeight}</span>}
+
       <input
         type="text"
         value={maxWeight}
         onChange={(e) => setMaxWeight(e.target.value)}
         placeholder="Max Weight"
       />
+      {errors.maxWeight && <span className="error">{errors.maxWeight}</span>}
+
       <input
         type="text"
         value={lifeSpan}
         onChange={(e) => setLifeSpan(e.target.value)}
         placeholder="Life Span"
       />
+      {errors.lifeSpan && <span className="error">{errors.lifeSpan}</span>}
+
       <input
         type="text"
         value={temperament}
         onChange={(e) => setTemperament(e.target.value)}
         placeholder="Temperament"
       />
-      <button type="submit">Create Dog Breed</button>
+      {errors.temperament && <span className="error">{errors.temperament}</span>}
+
+      {alertError && (
+        <div className="error-summary">
+          <p>Maximum value should be greater than the minimum value for height and weight. Modify the values</p>
+        </div>
+      )}
+
+      <button type="submit" disabled={!formValid}>
+        Create Dog Breed
+      </button>
+
+   
+
     </form>
   );
 };
